7b0ac318aca245206dc183936fad406d0f98406d
thingsym <thingsym@gmail.com>
2018-11-20 18:07:12 +0900

    perf: migrate functions.php

---
+++ functions/setup/class-theme.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * theme
+ *
+ * @package Pera
+ * @since 1.0.0
+ */
+
+namespace Pera\Setup;
+
+/**
+ * class Theme
+ *
+ * @since 1.0.0
+ */
+class Theme {
+	protected $hook_prefix = 'pera/functions/setup/';
+
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'setup' ) );
+		add_action( 'after_setup_theme', array( $this, 'content_width' ), 0 );
+	}
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * @since 1.0.0
+	 */
+	public function setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Pera, use a find and replace
+		 * to change 'pera' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'pera', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menu( 'menu-1', esc_html__( 'Primary', 'pera' ) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( $this->hook_prefix . 'custom-background/args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Set up the WordPress core custom header feature.
+		if ( class_exists( 'Pera\Functions\Custom_Header' ) ) {
+			global $custom_header;
+			add_theme_support( 'custom-header', apply_filters( $this->hook_prefix . 'custom-header/args', array(
+				'default-image'          => '',
+				'default-text-color'     => '000000',
+				'width'                  => 1000,
+				'height'                 => 250,
+				'flex-height'            => true,
+				'wp-head-callback'       => array( $custom_header, 'header_style' ),
+			) ) );
+		}
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+
+		// Adding support for core block visual styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Add support for full and wide align images.
+		add_theme_support( 'align-wide' );
+
+	}
+
+	/**
+	 * Set the content width in pixels, based on the theme's design and stylesheet.
+	 *
+	 * Priority 0 to make it available to lower priority callbacks.
+	 *
+	 * @global int $content_width
+	 */
+	public function content_width() {
+		// This variable is intended to be overruled from themes.
+		// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+		$GLOBALS['content_width'] = apply_filters( $this->hook_prefix . 'content_width', 640 );
+	}
+}
---
+++ functions/setup/class-widgets.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Widgets
+ *
+ * @package Pera
+ * @since 1.0.0
+ */
+
+namespace Pera\Setup;
+
+/**
+ * class Widgets
+ *
+ * @since 1.0.0
+ */
+class Widgets {
+
+	public function __construct() {
+		add_action( 'widgets_init', array( $this, 'init' ) );
+	}
+
+	/**
+	 * Register widget area.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+	 *
+	 * @since 1.0.0
+	 */
+	public function init() {
+
+		register_sidebar( array(
+			'name'          => esc_html__( 'Sidebar', 'pera' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'pera' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		) );
+
+	}
+
+}
