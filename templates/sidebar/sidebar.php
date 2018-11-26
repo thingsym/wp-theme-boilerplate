9ccd1a2c302fd370e090456a38487f0ccc3a7c79
thingsym <thingsym@gmail.com>
2018-11-27 02:25:05 +0900

    fix: fix html structure

--- templates/sidebar/sidebar.php
+++ templates/sidebar/sidebar.php
@@ -12,6 +12,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
+<div class="secondary">
+<div class="widget-area">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+</div>
+</div>
