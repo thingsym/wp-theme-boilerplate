b50b1257b3b23cb38ff9a819f34d7784c249fa72
thingsym <thingsym@gmail.com>
2019-01-29 18:44:10 +0900

    fix: fix text domain

--- templates/content/404.php
+++ templates/content/404.php
@@ -8,11 +8,11 @@
 ?>
 <section class="error-404 not-found">
 <header class="page-header">
-<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'pera' ); ?></h1>
+<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp-theme-boilerplate' ); ?></h1>
 </header><!-- .page-header -->
 
 <div class="page-content">
-<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pera' ); ?></p>
+<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp-theme-boilerplate' ); ?></p>
 
 <?php
 get_search_form();
@@ -21,7 +21,7 @@ the_widget( 'WP_Widget_Recent_Posts' );
 ?>
 
 <div class="widget widget_categories">
-<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'pera' ); ?></h2>
+<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wp-theme-boilerplate' ); ?></h2>
 <ul>
 	<?php
 	wp_list_categories(
@@ -39,7 +39,7 @@ the_widget( 'WP_Widget_Recent_Posts' );
 
 <?php
 /* translators: %1$s: smiley */
-$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'pera' ), convert_smilies( ':)' ) ) . '</p>';
+$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wp-theme-boilerplate' ), convert_smilies( ':)' ) ) . '</p>';
 the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
 the_widget( 'WP_Widget_Tag_Cloud' );
--- templates/parts/comments.php
+++ templates/parts/comments.php
@@ -32,13 +32,13 @@ if ( post_password_required() ) {
 			if ( '1' === $comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'pera' ),
+					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'wp-theme-boilerplate' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'pera' ) ),
+					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'wp-theme-boilerplate' ) ),
 					number_format_i18n( $comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
@@ -65,7 +65,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'pera' ); ?></p>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'wp-theme-boilerplate' ); ?></p>
 			<?php
 		endif;
 
