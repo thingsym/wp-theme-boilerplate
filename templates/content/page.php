56805a7127eadbc3c72cebb5725735d9eb7a1546
thingsym <thingsym@gmail.com>
2019-03-04 21:07:35 +0900

    fix: fix space

--- templates/content/page.php
+++ templates/content/page.php
@@ -35,7 +35,7 @@
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'wp-theme-boilerplate' ),
+						__( 'Edit<span class="screen-reader-text"> %s</span>', 'wp-theme-boilerplate' ),
 						array(
 							'span' => array(
 								'class' => array(),
