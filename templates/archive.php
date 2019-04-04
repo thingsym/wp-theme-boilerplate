fe384ee48e82b9c37fb8dcd099b3fa76ff562a7a
thingsym <thingsym@gmail.com>
2019-04-04 18:33:19 +0900

    perf: remove theme hook tags

--- templates/archive.php
+++ templates/archive.php
@@ -22,8 +22,6 @@
 </header>
 <?php do_action( 'wp_theme_boilerplate/theme_hook/site/header/after' ); ?>
 
-<?php do_action( 'wp_theme_boilerplate/theme_hook/navi/global' ); ?>
-
 <?php do_action( 'wp_theme_boilerplate/theme_hook/site/content/before' ); ?>
 <div class="site-content">
 <div class="primary">
@@ -57,10 +55,7 @@ do_action( 'wp_theme_boilerplate/theme_hook/content/append' );
 
 <?php do_action( 'wp_theme_boilerplate/theme_hook/site/footer/before' ); ?>
 <footer class="site-footer">
-<?php
-do_action( 'wp_theme_boilerplate/theme_hook/site/footer' );
-do_action( 'wp_theme_boilerplate/theme_hook/site/site_info' );
-?>
+<?php do_action( 'wp_theme_boilerplate/theme_hook/site/footer' ); ?>
 </footer>
 <?php do_action( 'wp_theme_boilerplate/theme_hook/site/footer/after' ); ?>
 </div>
