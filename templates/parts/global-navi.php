7e883a836352c8fa9012a857042e9398126847ae
thingsym <thingsym@gmail.com>
2019-04-04 16:18:19 +0900

    fix: change class name of menu

--- templates/parts/global-navi.php
+++ templates/parts/global-navi.php
@@ -11,9 +11,8 @@ wp_nav_menu(
 	array(
 		'theme_location'  => 'menu-1',
 		'container'       => 'nav',
-		'container_class' => 'global-menu',
-		'menu_class'      => 'menu',
+		'container_class' => 'site-navi',
+		'menu_class'      => 'global-menu',
 		'fallback_cb'     => false,
 	)
 );
-
