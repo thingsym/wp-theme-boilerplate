2fc89d89c787e6ea812ca0bbe44d6dc2bb9411fd
thingsym <thingsym@gmail.com>
2019-04-11 19:03:22 +0900

    perf: add has_* function for checking if valid

--- templates/parts/header-image.php
+++ templates/parts/header-image.php
@@ -6,6 +6,8 @@
  */
 
 ?>
+<?php if ( has_header_image() ) : ?>
 <div class="site-header-image">
 <img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="">
 </div>
+<?php endif; ?>
