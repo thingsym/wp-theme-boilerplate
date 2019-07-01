c32504272fac4065a22c75f94c94e2a7862f3919
thingsym <thingsym@gmail.com>
2019-07-01 19:53:06 +0900

    test: add test case

---
+++ tests/php/test-setup-theme.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Class Test_Setup_Theme
+ *
+ * @package WP Theme Boilerplate
+ */
+
+class Test_Setup_Theme extends WP_UnitTestCase {
+
+	public function setUp() {
+		parent::setUp();
+		$this->theme = new \WP_Theme_Boilerplate\Functions\Setup\Theme();
+	}
+
+	/**
+	 * @test
+	 * @group Theme
+	 */
+	public function constructor() {
+		$this->assertEquals( 10, has_filter( 'after_setup_theme', array( $this->theme, 'setup' ) ) );
+		$this->assertEquals( 0, has_filter( 'after_setup_theme', array( $this->theme, 'content_width' ) ) );
+		$this->assertEquals( 0, has_filter( 'wp_head', array( $this->theme, 'print_meta' ) ) );
+	}
+
+}
